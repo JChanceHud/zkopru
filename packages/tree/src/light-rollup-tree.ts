4257587439739322ca1790bf8addf5456bead0b3
Chance Hudson <jchancehud@gmail.com>
2021-03-03 00:32:23 -0600

    fix: ts compile errors

--- packages/tree/src/light-rollup-tree.ts
+++ packages/tree/src/light-rollup-tree.ts
@@ -549,5 +549,5 @@ export abstract class LightRollUpTree<T extends Field | BN> {
     return false
   }
 
-  abstract async indexesOfTrackingLeaves(): Promise<T[]>
+  abstract indexesOfTrackingLeaves(): Promise<T[]>
 }
